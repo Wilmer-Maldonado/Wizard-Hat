3299d2c9754407bd2a9b93353efbe9f72f6cf3a8
Wilmer-Maldonado <maldonadow2019@gmail.com>
2023-04-02 14:07:38 -0400

    greetings package

---
+++ greetings/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='greetings',
+      version='0.1',
+      description='The most basic greetings',
+      url='https://github.com/Wilmer-Maldonado/Wizard-Hat/greetings',
+      author='Wilmer Maldonado',
+      author_email='etc7fq@virginia.edu',
+      license='MIT',
+      packages=['greetings'])
